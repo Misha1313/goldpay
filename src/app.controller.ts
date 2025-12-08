2376b240955da9ff741a4d959d443f0ad331f0c3
m-marghishvili <m.marghishvili@adjarabet.com>
2025-12-09 01:46:45 +0400

    log

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,8 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import path from 'path';
+import fs from 'fs';
+import { Public } from './business/auth/decorators/public.decorator';
 
 @Controller()
 export class AppController {
@@ -13,5 +16,20 @@ export class AppController {
   @Get('/')
   getRoot() {
     return 'OK';
+  }
+
+  @Public()
+  @Get('files')
+  getFiles() {
+    const dir = path.join(process.cwd(), 'dist');
+    const walk = (dirPath: string): any => {
+      return fs.readdirSync(dirPath).map((file) => {
+        const filePath = path.join(dirPath, file);
+        const stat = fs.statSync(filePath);
+        return stat.isDirectory() ? { [file]: walk(filePath) } : file;
+      });
+    };
+
+    return walk(dir);
   }
 }
