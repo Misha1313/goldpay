403fa493b09fd1cc25e01bcaffeb8321cf1355e7
m-marghishvili <m.marghishvili@adjarabet.com>
2026-02-17 21:27:12 +0400

    fix bug

--- src/main.ts
+++ src/main.ts
@@ -21,6 +21,6 @@ async function bootstrap() {
   const documentFactory = () => SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('swagger', app, documentFactory);
 
-  await app.listen(3000);
+  await app.listen(3000, '0.0.0.0');
 }
 bootstrap();
