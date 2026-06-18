5f44360532368aba06153991563edd5572ae6b9e
misha marghishvili <margishvili16@gmail.com>
2026-06-19 01:31:03 +0400

    throw error in case of info error

--- src/business/transaction/transaction.service.ts
+++ src/business/transaction/transaction.service.ts
@@ -112,7 +112,7 @@ export class TransactionService {
           driverId,
           parkId,
         });
-        return;
+        throw new Error('Info');
       }
 
       const updateBalanceResponse =
